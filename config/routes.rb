e68edd9c18de7b81a20abe9b5d1a14cf86e42819
yuttaro <135433538+yuttaro@users.noreply.github.com>
2023-08-10 11:54:52 +0000

    Start

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # 顧客用
+# URL /customers/sign_in ...
+devise_for :customers, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, controllers: {
+  sessions: "admin/sessions"
+}
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
